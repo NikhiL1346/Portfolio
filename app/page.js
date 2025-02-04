aebf7c36e12828986cdd784b627666cf07d5306f
Nikhil Dayma <nikhildayma46@gmail.com>
2025-02-04 22:46:54 +0530

    services and work done

--- app/page.js
+++ app/page.js
@@ -2,12 +2,16 @@
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import About from './components/About';
+import Services from './components/Services';
+import Work from './components/Work';
 export default function Home() {
   return (
     <>
      <Navbar/>
      <Header/>
      <About/>
+     <Services/>
+     <Work/>
     </>
   );
 }
