8e13b328de5078fae2f47172053f56f217ebeb0c
Nikhil Dayma <nikhildayma46@gmail.com>
2025-02-26 17:06:33 +0530

    Completed Portfolio!!! YAY!

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import React, { useEffect, useRef, useState } from 'react'
 import { assets } from './../../assets/assets';
 
-const Navbar = () => {
+const Navbar = ({isDarkMode,setIsDarkMode}) => {
   const sideMenuRef =useRef();
   
   const openMenu = ()=>{
@@ -27,7 +27,7 @@ const Navbar = () => {
   return (
     <>
      <div className='fixed top-0 right-0 w-11/12
-      -z-10 translate-y-[-80%]'>
+      -z-10 translate-y-[-80%] dark:hidden'>
         <Image src={assets.header_bg_color} alt=""
         className='w-full'/>
      </div>
@@ -35,10 +35,10 @@ const Navbar = () => {
       
       <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%]
        py-4 flex items-center 
-       justify-between z-50 ${isScroll?"bg-white bg-opacity-50 backdrop-blur-lg shadow-sm"
-       : ""}`}>
+       justify-between z-50 ${isScroll?"bg-white bg-opacity-50 backdrop-blur-lg shadow-sm dark:bg-darkTheme dark:shadow-white/20 "
+       : ""} `}>
          <a href="#top"> {/* when clicked goes to the top of the page. */}
-            <Image src={assets.logo}
+            <Image src={isDarkMode?assets.logo_dark:assets.logo}
             alt='' 
             className='w-28 cursor-pointer mr-14'/>
         </a>
@@ -46,7 +46,7 @@ const Navbar = () => {
          gap-6 lg:gap-8 
          rounded-full px-12 py-3 
          ${isScroll?""
-          :" bg-white shadow-sm bg-opacity-50"}
+          :" bg-white shadow-sm bg-opacity-50 dark:border dark:border-white/50 dark:bg-transparent"}
         `}>
             <li><a className='font-Ovo' href="#top">Home</a></li>
             <li><a className='font-Ovo' href="#about">About Me</a></li>
@@ -58,30 +58,31 @@ const Navbar = () => {
         
         <div className='flex items-center gap-4'>
         
-        <button>
-            <Image src={assets.moon_icon} alt='' 
+        <button onClick={()=>setIsDarkMode(val=>!val)}>
+            <Image src={isDarkMode ?assets.sun_icon :assets.moon_icon} alt='' 
             className='w-6'/>
         </button>
             
             <a href="#contact"
              className='hidden lg:flex items-center gap-3 px-10
-             py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo'>Contact 
-             <Image src={assets.arrow_icon}
+             py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo
+             dark:border-white/50'>Contact 
+             <Image src={isDarkMode?assets.arrow_icon_dark:assets.arrow_icon}
              alt=''
              className=' w-3'/>
             </a>
             <button className='block md:hidden ml-3'  onClick={openMenu}>
-                <Image src={assets.menu_black} alt=''
+                <Image src={isDarkMode ?assets.menu_white:assets.menu_black} alt=''
                 className='w-6'/>
             </button>
         </div>
         {/* mobile menu */}
         
         <ul ref ={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64
-        top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500'>
+        top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500 dark:bg-darkHover dark:text-white'>
         
         <div className='absolute right-6 top-6' onClick={closeMenu}>
-            <Image src={assets.close_black} alt=''
+            <Image src={isDarkMode?assets.close_white:assets.close_black} alt=''
              className='cursor-pointer w-5'/>
         </div>
 
--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -1,47 +1,79 @@
 import { assets, workData } from "@/assets/assets";
 import Image from "next/image";
 import React from "react";
+import {motion} from 'motion/react';
 
-const Work = () => {
+const Work = ({isDarkMode}) => {
   return (
-    <div id='work' className="w-full px-[12%] py-10 scroll-mt-20 ">
-      <h4 className="text-center mb-2 text-lg font-Ovo ">My Portfolio</h4>
-      <h2 className="text-center text-5xl font-Ovo">My latest Work</h2>
+    <motion.div 
+    initial={{opacity:0}}
+    whileInView={{opacity:1}}
+    transition={{duration:1}}
+    id='work' className="w-full px-[12%] py-10 scroll-mt-20 ">
+      <motion.h4 
+      initial={{opacity:0,y:-20}}
+      whileInView={{opacity:1,y:0}}
+      transition={{duration:0.5,delay:0.3}}
+      className="text-center mb-2 text-lg font-Ovo ">My Portfolio</motion.h4>
+      <motion.h2 
+   initial={{opacity:0,y:-20}}
+   whileInView={{opacity:1,y:0}}
+   transition={{duration:0.5,delay:0.5}}
+      className="text-center text-5xl font-Ovo">My latest Work</motion.h2>
     
     
-    <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">
+    <motion.p
+    initial={{opacity:0}}
+    whileInView={{opacity:1}}
+    transition={{duration:0.5,delay:0.7}}
+    className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">
      SOme text
-    </p>
+    </motion.p>
 
-    <div className="grid grid-cols-auto my-10 gap-5">
+    <motion.div 
+    initial={{opacity:0}}
+    whileInView={{opacity:1}}
+    transition={{duration:0.6,delay:0.9}}
+    className="grid grid-cols-auto my-10 gap-5 dark:text-black">
         {workData.map((project,index)=>(
-            <div key={index} 
+            <motion.div 
+           
+      whileHover={{scale:1.05}}
+      transition={{duration:0.3}}
+            key={index} 
             className="aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative
             cursor-pointer group"
             style={{backgroundImage:`url(${project.bgImage})` 
             }}>
-               <div className="bg-white w-10/12 rounded-md absolute 
+               <motion.div className="bg-white w-10/12 rounded-md absolute 
                bottom-5 left-1/2  -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500
                group-hover:bottom-7">
 
-               <div >
-                   <h2 className="font-semibold">{project.title}</h2>
+               <motion.div >
+                   <motion.h2 className="font-semibold">{project.title}</motion.h2>
                    <p className="text-sm text-gray-700">{project.description}</p>
-               </div>
-               <div className="border rounded-full border-black w-9 aspect-square flex
+               </motion.div>
+               <motion.div className="border rounded-full border-black w-9 aspect-square flex
                items-center justify-center shadow-[2px_2_0_#000] hover:group-hover:bg-lime-300 transition">
                    <Image src={assets.send_icon} alt="send icon"
                     className="w-5"/>
-               </div>
+               </motion.div>
                <button>Show More</button>
-               </div>
-            </div>
+               </motion.div>
+            </motion.div>
         ))}
-    </div>
-    <a href="" className="w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500">Show more<Image src={assets.right_arrow_bold} alt="right arrow" 
+    </motion.div>
+    <motion.a 
+    initial={{opacity:0}}
+    whileInView={{opacity:1}}
+    transition={{duration:0.5,delay:1.1}}
+    href="" className="w-max flex items-center justify-center gap-2 text-gray-700
+     border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20
+      hover:bg-lightHover duration-500 dark:text-white dark:border-white dark:hover:bg-darkHover">Show more
+      <Image src={isDarkMode?assets.right_arrow_bold_dark:assets.right_arrow_bold} alt="right arrow" 
     className="w-4"/>
-    </a>
-    </div>
+    </motion.a>
+    </motion.div>
   );
 };
 
